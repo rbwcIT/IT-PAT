90d1e19ca26d731dc96ef38d49e099a9a1003cd0
rbwcIT <42063888+rbwcIT@users.noreply.github.com>
2018-08-14 18:54:12 +0200

    Add files via upload

--- Client Works/SocketClient1_U.pas
+++ Client Works/SocketClient1_U.pas
@@ -34,7 +34,7 @@ var
 const
   MOVEMENT_SPACE : Integer = 10;
   SERVER_IP : string = '127.0.0.1';     // by huis en basic testing
-  //SERVER_IP : string = '192.168.0.102'; // by skool en network testing
+  //SERVER_IP : string = '192.168.0.1'; // by skool en network testing
   CONNECTION_PORT : Integer = 2015;     // makliker as ons later wil verander
 
 implementation
@@ -46,37 +46,34 @@ procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word;
 var
   sMovement : string;   // format: <direction> <position>
   Direction : Char;
-  sprite : TImage;
 begin
-
-  if MY_CONNECTION_NUMBER = '0' then                // kyk net watte image is wie sin mar ons gan later dynamic array werk
-    sprite := imgClient1
-  else
-    sprite := imgClient2;
-
   case key of
   VK_UP:
-    Sprite.Top := Sprite.Top - MOVEMENT_SPACE;
+    Direction := 'U';
 
   VK_DOWN:
-    Sprite.Top := Sprite.Top + MOVEMENT_SPACE;       // kort dit as deel van movement string
+    Direction := 'D';       // kort dit as deel van movement string
 
   VK_LEFT:
-    Sprite.Left := Sprite.Left - MOVEMENT_SPACE;
+    Direction := 'L';
 
   VK_RIGHT:
-    Sprite.Left := Sprite.Left + MOVEMENT_SPACE;
+    Direction := 'R';
   end;
 
- 
-  clntsckt1.Socket.SendText(MY_CONNECTION_NUMBER + '#' + IntToStr(sprite.Left) + '|' + IntToStr(sprite.Top));
+  if MY_CONNECTION_NUMBER = '0' then                // kyk net watte image is wie sin mar ons gan later dynamic array werk
+    sMovement := BuildInstruction(imgClient1, Direction)
+  else
+    sMovement := BuildInstruction(imgClient2, Direction);
+
+  clntsckt1.Socket.SendText(sMovement);
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
-  clntsckt1.Host := SERVER_IP ;   // verander die constant bo
+  clntsckt1.Host := SERVER_IP ;   // verander die constant bo 
   clntsckt1.Port := CONNECTION_PORT;
-  clntsckt1.Active := True ;
+  clntsckt1.Active := True ; 
 end;
 
 procedure TForm1.clntsckt1Connect(Sender: TObject;
@@ -97,7 +94,7 @@ begin
     MY_CONNECTION_NUMBER := sText;
     Exit;
   end;
-
+  
 //  ons gan later n dynamic array gebruik om al die connectons te log en die images daaran te koppel
   moving_connection := Copy(sText, 1, Pos('#', sText)-1);
   Delete(sText, 1, Pos('#', sText));
@@ -105,32 +102,56 @@ begin
   if moving_connection = '0' then  // temporary image assignment
     MoveSprite(imgClient1, sText)
   else
-    MoveSprite(imgClient2, sText);
+    MoveSprite(imgClient2, sText);    
 end;
 
 procedure TForm1.MoveSprite(Sprite: TImage; instructions: string);   // skuif image wat jy se na sekere plek toe
+var
+  sDirection : string;
 begin
-  Sprite.Left := StrToInt(Copy(instructions, 1, Pos('|', instructions)-1));
-  Sprite.Top := StrToInt(Copy(instructions, Pos('|', instructions)+1, Length(instructions))) ;
+  sDirection := instructions[1];
+  Delete(instructions, 1, 1);
+
+  if ((sDirection='U') or (sDirection='D')) then
+    Sprite.Top := StrToInt(instructions)
+  else
+    Sprite.Left := StrToInt(instructions);
 end;
 
-function TForm1.BuildInstruction(Sprite: TImage;  //  format: <connection_number>  # <x waarde> | <y waarde>
+function TForm1.BuildInstruction(Sprite: TImage;  //  format: <connection_number>  # <direction(char)> <position>
   cDirection: char): string;
 var
   position : Integer;
 begin
-  Result := MY_CONNECTION_NUMBER + '#';
-
+  Result := MY_CONNECTION_NUMBER + '#' + cDirection;
 
   case cDirection of
-  'U':  Sprite.Top := Sprite.Top - MOVEMENT_SPACE;
-  'D':  Sprite.Top := Sprite.Top + MOVEMENT_SPACE;
-  'L':  Sprite.Left := Sprite.Left - MOVEMENT_SPACE;
-  'R':  Sprite.Left := Sprite.Left + MOVEMENT_SPACE;
+  'U':
+    begin
+      Sprite.Top := Sprite.Top - MOVEMENT_SPACE;
+      position := Sprite.Top;
+    end;
+  'D':
+    begin                                                  // skuif image en record position na geskuif is
+      Sprite.Top := Sprite.Top + MOVEMENT_SPACE;
+      position := Sprite.Top;
+    end;
+  'L':
+    begin
+      Sprite.Left := Sprite.Left - MOVEMENT_SPACE;
+      position := Sprite.Left;
+    end;
+  'R':
+    begin
+      Sprite.Left := Sprite.Left + MOVEMENT_SPACE;
+      position := Sprite.Left;
+    end;
   end;
 
-  Result := Result + IntToStr(Sprite.Left) + '|' + IntToStr(Sprite.Top);
-  lbl2.Caption:= Result;   // <-- debugging  
+  Result := Result + IntToStr(position);
+  lbl2.Caption:= Result;   // <-- debugging
+  
+
 end;
 
 end.
